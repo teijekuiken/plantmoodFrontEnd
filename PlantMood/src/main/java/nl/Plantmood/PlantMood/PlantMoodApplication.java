18c7c9004fb77b13acc8dbf3ae5672093cc26988
Kuiken,Teije T.P.K <t.kuiken@student.fontys.nl>
2020-03-26 22:39:20 +0100

    Initial commit

---
+++ PlantMood/src/main/java/nl/Plantmood/PlantMood/PlantMoodApplication.java
@@ -0,0 +1,13 @@
+package nl.Plantmood.PlantMood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantMoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantMoodApplication.class, args);
+	}
+
+}
