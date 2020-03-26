6e5957f0664d7e7fd0b4f5b2c59d6b8345cfb670
Kuiken,Teije T.P.K <t.kuiken@student.fontys.nl>
2020-03-26 22:20:30 +0100

    Initial commit

---
+++ src/test/java/nl/Plantmood/PlantMood/PlantMoodApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.Plantmood.PlantMood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantMoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
