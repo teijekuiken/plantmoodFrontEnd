e7ede41ff75316cca6dd26c50ee44388ba197b31
Vogel,Menno M <menno.vogel@student.fontys.nl>
2020-05-11 15:40:05 +0200

    added logger in Callback and reconnect to mosquitto when connection is lost

--- domain/src/main/java/com/oopa/domain/mqtt/Callback.java
+++ domain/src/main/java/com/oopa/domain/mqtt/Callback.java
@@ -3,16 +3,25 @@ package com.oopa.domain.mqtt;
 import com.oopa.domain.services.MqttService;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Callback implements MqttCallback {
 
+    private static Logger logger = LoggerFactory.getLogger(Callback.class);
+    private Mqtt mqtt;
     private MqttService mqttService;
 
     @Override
     public void connectionLost(Throwable throwable){
-        // TODO: 27/04/2020 add logger instead of println
-        System.out.println("Connection to MQTT broker is lost");
+        logger.error("Connection to MQTT broker is lost");
+        try {
+            mqtt.startMqtt();
+        } catch (MqttException e) {
+            logger.error(e.toString());
+        }
     }
 
     @Override
