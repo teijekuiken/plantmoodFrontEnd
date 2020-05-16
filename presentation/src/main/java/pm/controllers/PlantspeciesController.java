6f644b158806e5f594cb593787a0d2b9c327fe00
Zanden,Alain A.W.M. van der <alain.vanderzanden@student.fontys.nl>
2020-05-16 15:18:45 +0200

    Update plant species

--- presentation/src/main/java/pm/controllers/PlantspeciesController.java
+++ presentation/src/main/java/pm/controllers/PlantspeciesController.java
@@ -1,41 +1,36 @@
 package pm.controllers;
 
+import com.oopa.domain.model.PlantSpecies;
 import com.oopa.domain.services.PlantSpeciesService;
-import com.oopa.interfaces.model.IPlantSpecies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RequestMapping("api/v1/plantspecies")
 @RestController
 public class PlantspeciesController {
 
+    @Autowired
     private PlantSpeciesService plantspeciesService;
 
-    public PlantspeciesController(){
-        this.plantspeciesService = new PlantSpeciesService();
-    }
-
     @PostMapping
-    public void addPlantSpecies(@RequestBody IPlantSpecies plantSpecies){
-        plantspeciesService.addPlantSpecies(plantSpecies);
+    public @ResponseBody PlantSpecies addPlantSpecies(@RequestBody PlantSpecies plantSpecies){
+        return plantspeciesService.addPlantSpecies(plantSpecies);
     }
 
     @GetMapping
-    public List<IPlantSpecies> getAllPlantSpecies(){
-        List<IPlantSpecies> plantSpecies = plantspeciesService.getAllPlantSpecies();
-        return plantSpecies;
+    public @ResponseBody List<PlantSpecies> getAllPlantSpecies(){
+        return plantspeciesService.getAllPlantSpecies();
     }
 
     @GetMapping(path = {"id"})
-    public Optional<IPlantSpecies> getPlantSpeciesById(@PathVariable("id") int id){
-        Optional<IPlantSpecies> plantspecies = plantspeciesService.getPlantSpeciesById(id);
-        return plantspecies;
+    public @ResponseBody PlantSpecies getPlantSpeciesById(@PathVariable("id") int id){
+        return plantspeciesService.getPlantSpeciesById(id);
     }
 
     @DeleteMapping
-    public void deletePlantSpecies(@PathVariable int id){
-        plantspeciesService.deletePlantSpecies(id);
+    public @ResponseBody PlantSpecies deletePlantSpecies(@PathVariable Integer id){
+        return plantspeciesService.deletePlantSpecies(id);
     }
 }
