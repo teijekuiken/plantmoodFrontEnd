894c3dc45c5bbe425ec5a16e0e5c31976f73c9e0
Tijn <tijnvdeijnde@outlook.com>
2020-05-23 20:34:51 +0200

    Basic Algoritme

--- domain/src/main/java/com/oopa/domain/services/PlantmoodService.java
+++ domain/src/main/java/com/oopa/domain/services/PlantmoodService.java
@@ -1,6 +1,7 @@
 package com.oopa.domain.services;
 
 import com.oopa.dataAccess.repositories.PlantmoodRepository;
+import com.oopa.domain.model.PlantSpecies;
 import com.oopa.domain.model.Plantmood;
 import com.oopa.interfaces.model.IPlantmood;
 import com.oopa.interfaces.model.IPlantmoodhistory;
@@ -10,11 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class PlantmoodService {
+
+    private LocalDateTime lastTestTime;
+
+    public PlantmoodService() {
+        lastTestTime = LocalDateTime.now();
+    }
+
     @Autowired
     private ModelMapper modelMapper;
 
@@ -38,9 +48,36 @@ public class PlantmoodService {
 
     }
 
-    public void getPlantStatus(IPlantmoodhistory plantmoodHistory, int plantspeciesId) {
+    public void getPlantStatus(List<IPlantmoodhistory> plantmoodhistories, PlantSpecies plantspecies) {
+        if (plantmoodhistories.size() > 4 && tenMinutesArePassed(lastTestTime)) {
+            double valueOfPlantmoodData = 0;
+            double avarageOfPlantmoodData = 0;
+            double multiplier = 1;
+            double substractionOfAverage = 0;
 
+            lastTestTime = LocalDateTime.now();
+
+            plantmoodhistories.sort(Comparator.comparing(IPlantmoodhistory::getCreatedAt));
+            List<IPlantmoodhistory> subListPlantmoodhistories = plantmoodhistories.stream()
+                    .sorted(Comparator.comparing(IPlantmoodhistory::getCreatedAt).reversed())
+                    .collect(Collectors.toList()).subList(0, 5);
+
+            for (IPlantmoodhistory history: subListPlantmoodhistories) {
+                valueOfPlantmoodData += multiplier * history.getHealth();
+                substractionOfAverage += multiplier;
+                multiplier -= 0.2;
+            }
+            avarageOfPlantmoodData = valueOfPlantmoodData / substractionOfAverage;
+
+            if (avarageOfPlantmoodData < plantspecies.getMinHumidity()) {
+                System.out.println("Geef water");
+            } else if (avarageOfPlantmoodData > plantspecies.getMaxHumidity()) {
+                System.out.println("Te veel water");
+            }
+            System.out.println("De plant is op het juiste niveau");
+        }
     }
+
 
     public Plantmood getPlantmoodById(Integer id) {
         var plantmood = plantmoodRepository.findById(id);
@@ -66,5 +103,10 @@ public class PlantmoodService {
         plantmoodRepository.deleteById(id);
         return this.modelMapper.map(plantmood, Plantmood.class);
     }
+
+    private boolean tenMinutesArePassed(LocalDateTime lastCheck) {
+        return lastCheck.isBefore(LocalDateTime.now().minusMinutes(10));
+    }
+
 
 }
