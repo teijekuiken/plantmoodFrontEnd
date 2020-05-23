c5d22536f81eb9224b2c7be82ddbebb9d9648925
Vogel,Menno M <menno.vogel@student.fontys.nl>
2020-05-23 17:29:43 +0200

    removed handler from MqttService

--- domain/src/main/java/com/oopa/domain/services/MqttService.java
+++ domain/src/main/java/com/oopa/domain/services/MqttService.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageHandler;
 import org.springframework.stereotype.Service;
 
 @Service
