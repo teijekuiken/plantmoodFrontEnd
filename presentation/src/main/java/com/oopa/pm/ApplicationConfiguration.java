885f85374db44085f0a2ac1aff6f57e1e27156c3
Zanden,Alain A.W.M. van der <alain.vanderzanden@student.fontys.nl>
2020-05-23 12:46:17 +0200

    Updated config

--- presentation/src/main/java/com/oopa/pm/ApplicationConfiguration.java
+++ presentation/src/main/java/com/oopa/pm/ApplicationConfiguration.java
@@ -1,6 +1,10 @@
 package com.oopa.pm;
 
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +13,16 @@ public class ApplicationConfiguration {
     @Bean(name = "modelMapper")
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Value("${oopa.mqtt.client}")
+    private String mqttClient;
+
+    @Value("${oopa.mqtt.client.id}")
+    private String clientId;
+
+    @Bean
+    public IMqttAsyncClient client() throws MqttException {
+        return new MqttAsyncClient(mqttClient, clientId);
     }
 }
